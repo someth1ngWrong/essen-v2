d52a805ad92f9a57ea6e26d6205b9091a8dd7885
Anatoly Nikiforov <tniks.work@gmail.com>
2021-06-26 03:58:47 +0300

    fix db

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,8 @@ ActiveRecord::Schema.define(version: 2021_06_24_215956) do
 
   create_table "collections", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "name"
+    t.string "name"
+    t.string "title"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -57,15 +58,6 @@ ActiveRecord::Schema.define(version: 2021_06_24_215956) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "main_id"
     t.index ["main_id"], name: "index_ingredients_on_main_id"
-  end
-
-  create_table "ingredients_recipes", force: :cascade do |t|
-    t.integer "recipe_id"
-    t.integer "ingredient_id"
-    t.integer "amount"
-    t.string "measure"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "post_ingredients", force: :cascade do |t|
@@ -103,20 +95,6 @@ ActiveRecord::Schema.define(version: 2021_06_24_215956) do
     t.integer "complexity"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "image"
-    t.integer "collection_id"
-    t.integer "category_id"
-    t.integer "complexity"
-    t.integer "cooking_time"
-    t.integer "number_of_servings"
-    t.string "recipe_text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
